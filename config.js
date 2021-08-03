a910bba236bd7fa0b515398189ab2c05eebf65e9
Maingron <robin@maingron.com>
2021-08-03 19:16:30 +0200

    Add comments to dummyblock

--- config.js
+++ config.js
@@ -14,17 +14,16 @@ if(localStorage.getItem("config1")) {
         "editticktime": (1000 / 2), // ms
         "prodticktime": (1000 / 60), // ms
         "ticktime": config.prodticktime, // ms
-        "background": "#000",
-        "lang": "en",
+        "background": "#000", // Background Color of the entire clock
+        "lang": "en", // Language
 
-        "dummyblock": {
-            "block": "",
-            "color": "#ffff00",
-            "background": "transparent",
-            "fontsize": "2rem",
-            "position": ["2rem","4.5rem"], // [x,y]
-            "scale": 1,
-            "id": 9999
+        "dummyblock": { // Dummy block - will be used when adding a new Block
+            "block": "", // Block Name (Example: block-hours) (DUMMY)
+            "color": "#ffff00", // Font color
+            "background": "transparent", // Block background color
+            "fontsize": "2rem", // Font size
+            "position": ["32px","70px"], // Position of the block // [x,y]
+            "id": 9999 // nth block (DUMMY)
         },
 
         // clocks
